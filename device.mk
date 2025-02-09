5c938b3a6abbe33e840b8683b3a6e3831e67bfde
lahaina <nattolecats@gmail.com>
2025-02-09 21:19:18 +0900

    eqe: Explicitly include hardware/motorola

--- device.mk
+++ device.mk
@@ -386,6 +386,7 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/motorola \
     kernel/motorola/sm7550 \
     kernel/motorola/sm7550-modules
 
